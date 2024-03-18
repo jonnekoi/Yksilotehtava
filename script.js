d2540462ba007cec1c14f7a25293aeae95007f29
jonnek <jonnekoivisto00@gmail.com>
2024-03-18 19:18:49 +0200

    lot of updates

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+const map = L.map('map');
+
+map.locate({setView: true, maxZoom: 16});
+map.on('locationfound', function(e) {
+    map.setView(e.latlng, 13);
+    L.marker(e.latlng).addTo(map);
+});
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright"></a>',
+}).addTo(map);
+
+const loginButton = document.getElementById("loginButton");
+const loginForm = document.getElementById("loginForm");
+const registerButton = document.getElementById("registerButton");
+const registerForm = document.getElementById("registerForm");
+
+loginButton.onclick = function (){
+    console.log("login button clicked!")
+    loginForm.style.display = 'block';
+    loginButton.style.display = 'none';
+    registerButton.style.display = 'none';
+}
+
+registerButton.onclick = function (){
+    console.log("register button clicked!")
+    registerForm.style.display = 'block';
+    loginButton.style.display = 'none';
+    registerButton.style.display = 'none';
+}
