e7ee8de2c7214f20d60df1337e93cbcfed40613d
jonnek <jonnekoivisto00@gmail.com>
2024-03-20 14:51:13 +0200

    language update for menu, added markers, lightmode fixes, popup icons.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const map = L.map('map');
+
 const loginButton = document.getElementById("loginButton");
 const loginForm = document.getElementById("loginForm");
 const registerButton = document.getElementById("registerButton");
@@ -13,19 +14,34 @@ const bodyElement = document.querySelector("body");
 const htmlElement = document.querySelector("html");
 const boxElement = document.querySelectorAll(".box");
 const buttons = document.querySelectorAll(".button");
+const weather = document.querySelector(".weather");
+const headerText = document.querySelector(".headerText");
+const menuHeader  = document.querySelector(".menuHeader");
+const logo = document.getElementById("logo");
+const languageButton = document.getElementById("languageButton");
+const flag = document.getElementById("flag");
 let isLight = false;
+let language = false;
+let kieli = 'fi';
 
 map.locate({setView: true, maxZoom: 16});
 map.on('locationfound', function (e) {
     map.setView(e.latlng, 13);
-    L.marker(e.latlng).addTo(map);
+    const myIcon = L.icon({
+        iconUrl: 'Resources/UserLocationIcon.png',
+        iconSize: [41, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [8, -41]
+    });
+    L.marker(e.latlng, {icon: myIcon}).addTo(map).bindPopup("Your location");
 });
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright"></a>',
 }).addTo(map);
 
-document.addEventListener('DOMContentLoaded', function (){
-    getUserLocation();
+document.addEventListener('DOMContentLoaded', async function (){
+    await getUserLocation();
+    await getRestaurants();
 })
 
 lightButton.onclick = function (){
@@ -36,10 +52,18 @@ lightButton.onclick = function (){
         })
         Array.from(buttons).forEach(buttons => {
             buttons.style.backgroundColor = '#B0A695';
+            buttons.style.color = 'black';
         })
+        logo.src = 'Resources/cutlery.png';
         lightButton.style.backgroundColor = '#B0A695';
+        languageButton.style.backgroundColor = '#B0A695';
+        lightButton.style.color = 'black';
         bodyElement.style.backgroundColor = '#B0A695';
         htmlElement.style.backgroundColor = '#B0A695';
+        headerText.style.color = "black";
+        weather.style.color = 'black';
+        menuHeader.style.color = 'black';
+        document.body.classList.add('light-mode');
         this.innerHTML = "&#9790;";
         isLight = true;
     }else{
@@ -49,10 +73,18 @@ lightButton.onclick = function (){
         })
         Array.from(buttons).forEach(buttons => {
             buttons.style.backgroundColor = '#2C3639';
+            buttons.style.color = 'white';
         })
+        logo.src = 'Resources/cutlery_white.png';
         lightButton.style.backgroundColor = '#2C3639';
+        lightButton.style.color = 'white';
+        languageButton.style.backgroundColor = '#2C3639';
         bodyElement.style.backgroundColor = '#2C3639';
         htmlElement.style.backgroundColor = '#2C3639';
+        headerText.style.color = "white";
+        weather.style.color = 'white';
+        menuHeader.style.color = 'white';
+        document.body.classList.remove('light-mode');
         this.innerHTML = "&#9728;";
         isLight = false;
     }
@@ -82,6 +114,23 @@ returnButtonLogin.onclick = function () {
     registerButton.style.display = 'block';
 }
 
+languageButton.onclick = function (){
+    if(!language){
+        flag.src = 'Resources/fi.svg';
+        language = true;
+        kieli = 'en';
+    }else{
+        flag.src = 'Resources/gb-eng.svg';
+        language = false;
+        kieli = 'fi';
+    }
+    const popup = map._popup;
+    if (popup) {
+        const id = popup._source.options.restaurantId;
+        getMenu(id, kieli);
+    }
+}
+
 function getWeather(lat, lon){
     const api = "ce77882036e7ff2d906c8535496d7fd9";
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}`;
@@ -106,7 +155,7 @@ function showWeather(data){
                             <p>${convert.toFixed(1)}°C</p>`;
 }
 
-function getUserLocation() {
+async function getUserLocation() {
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(function(position) {
             const lat = position.coords.latitude;
@@ -114,4 +163,59 @@ function getUserLocation() {
             getWeather(lat, lon);
         });
     }
+}
+
+
+async function getRestaurants() {
+    const url = "https://10.120.32.94/restaurant/api/v1/restaurants/?name";
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(item => {
+                const coordinates = item.location.coordinates;
+                const latitude = coordinates[0];
+                const longitude = coordinates[1];
+                const id = item._id;
+                const marker = L.marker([longitude, latitude], {restaurantId: id}).addTo(map).bindPopup(item.name);
+                marker.on('popupopen', ()=> {
+                    getMenu(id, kieli);
+                });
+            })
+        })
+}
+
+async function getMenu(id, kieli) {
+    const urlMenu = `https://10.120.32.94/restaurant/api/v1/restaurants/daily/${id}/${kieli}`;
+    fetch(urlMenu)
+        .then(response => response.json())
+        .then(data => {
+            const menuDiv = document.getElementById("restaurantMenu");
+            menuDiv.innerHTML = '';
+
+            if (data.courses && Array.isArray(data.courses) && data.courses.length > 0) {
+                data.courses.forEach(item => {
+                    const menuItem = document.createElement('p');
+                    const priceText = item.price ? `: ${item.price}` : ' - Price not available';
+                    menuItem.textContent = `${item.name}${priceText}`;
+                    if(!isLight){
+                        menuItem.style.color = 'white';
+                    }else {
+                        menuItem.style.color = 'black';
+                    }
+                    menuDiv.appendChild(menuItem);
+                });
+            } else {
+                const noMenuAvailable = document.createElement('p');
+                noMenuAvailable.textContent = 'No menu available';
+                if(!isLight){
+                    noMenuAvailable.style.color = 'white';
+                }else {
+                    noMenuAvailable.style.color = 'black';
+                }
+                menuDiv.appendChild(noMenuAvailable);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
